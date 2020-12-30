fcec5d9dd1a5e3c4dc8516e3081a523a9d7d9259
KtyJIxy <48325606+KtyJIxy@users.noreply.github.com>
2020-12-30 21:10:13 +0200

    GUI starters

---
+++ GUI.py
@@ -0,0 +1,9 @@
+"""
+Place, where Graphic User Interface comes to life.
+"""
+
+from tkinter import * #To avoid unused import errors import only required.
+from Calc.functions_main import * #Same
+
+#To use GUI-tkinter use class constructs. Remember __init__!
+#Steal Lutz logic and make changes
