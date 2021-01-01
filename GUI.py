6addabd65818515caa98dd11d724710186deab89
KtyJIxy <48325606+KtyJIxy@users.noreply.github.com>
2021-01-01 19:58:36 +0200

    GUI. Found a way to run all importing module functions at once - will allow great customizability. If works. Need to start real UI.

--- GUI.py
+++ GUI.py
@@ -4,6 +4,18 @@ Place, where Graphic User Interface comes to life.
 
 from tkinter import * #To avoid unused import errors import only required.
 from Calc.functions_main import * #Same
+from Calc.functions_main import * #To avoid unused import errors import only required.
 
 #To use GUI-tkinter use class constructs. Remember __init__!
 #Steal Lutz logic and make changes
+
+class MainWindow(Frame): #Inherits tkinter Frame-class, builds up a main program window
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.master.title('PPCC')
+        
+    def invoke_basic_functions(self): #Activates all functions from functions_main.py
+        for functions in dir(Calc.functions_main):
+            function = getattr(Calc.functions_main, functions)
+            if callable(function):
+                function() #Requires re-work to invoke actual buttons
