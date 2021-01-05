2aa40fd98e5777a3930ec4fabaa6ed5923571a1d
KtyJIxy <48325606+KtyJIxy@users.noreply.github.com>
2021-01-05 22:56:49 +0200

    Extensions for better readability in VSC activated. Code reformatted accordingly.

--- GUI.py
+++ GUI.py
@@ -2,33 +2,33 @@
 Place, where Graphic User Interface comes to life.
 """
 
-#from tkinter import * #To avoid unused import errors import only required.
+#from tkinter import * #?To avoid unused import errors import only required.
 import tkinter as tk
-from Calc.functions_main import * #Same
-#from Calc.functions_main import * #To avoid unused import errors import only required.
+from Calc.functions_main import * #?Same
+#from Calc.functions_main import * #?To avoid unused import errors import only required.
 
-#To use GUI-tkinter use class constructs. Remember __init__!
-#Steal Lutz logic and make changes
+# TODO:To use GUI-tkinter use class constructs. Remember __init__!
+# TODO: Steal Lutz logic and make changes
 
 class MainWindow(Frame): #Inherits tkinter Frame-class, builds up a main program window
     def __init__(self, parent=None):
         #Frame.__init__(self, parent)
-        super().__init__(master) #Don't like calling the super(), but it was in example
+        super().__init__(master) # ? Don't like calling the super(), but it was in example
         self.master = master
         self.master.title('PPCC')
         self.pack()
-        self.invoke_basic_functions() #calling custom function
+        self.invoke_basic_functions() # * calling custom function
         
     def invoke_basic_functions(self): #Activates all functions from functions_main.py
         import Calc.functions_main
         for functions in dir(Calc.functions_main):
             function = getattr(Calc.functions_main, functions)
             if callable(function):
-                self.func() = tk.Button(self) #Doesn't work. Can't assign for some reason.
+                self.func() = tk.Button(self) # ! Doesn't work. Can't assign for some reason.
                 self.func["text"] = "%function_name" %function
                 self.func["command"] = self.function()
                 self.func.pack(side="top")
 
 root = tk.Tk()
 app = Application(master=root)
-app.mainloop() #From example. Required to startup tkinter-gui.
+app.mainloop() # ? From example. Required to startup tkinter-gui.
