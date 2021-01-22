9f912f7bc5d459adba71ebac4787a30cc1d1e3ef
KtyJIxy <48325606+KtyJIxy@users.noreply.github.com>
2021-01-22 22:06:31 +0200

    Real start of Kivy_GUI

--- Kivy_GUI.py
+++ Kivy_GUI.py
@@ -20,35 +20,23 @@
 #? p.unknown RstDocument - solution to editable PyReader!
 
 import kivy
-import time #* Test purposes
 kivy.require('2.0.0') #! Current is 2.0.0. as of 08/01/2021.
-from kivy.app import App #* Example code below
+from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from Calc.functions_main import *
 
-class MainScreen(GridLayout): #TODO high possibility of not working as intended due to __wrong__ __class__ inheritance.
+class MainScreen(GridLayout):
     def __init__(self, **kwargs):
-        super(Calc, self).__init__(**kwargs) #* I fucking hate __super__ calls
-        self.cols = 2 #! No, more. Plan.
-        self.rows = 2 #! Purely, my guess. Check.
-        #*Nice to know such commands exist. 
-        #! Delete in future
-        #self.add_widget(Label(text='User Name')) 
-        #self.username = TextInput(multiline=False)
-        #self.add_widget(self.username)
-        #self.add_widget(Label(text='password'))
-        #self.password = TextInput(password=True, multiline=False)
-        #self.add_widget(self.password)
-
-class Calc(App):
-    def build(self):
-        #return Label(text='Hello world') #? Example code. Label being root widget application.
-        #! import functions_main.py -- GO BACK TO __MainScreen__
-        time.sleep(20)
-        return MainScreen()
-        time.sleep(20)
+        super(Calc, self).__init__(**kwargs)
         
+class CalcOutputScreen(Widget):
+    pass
+class CalcApp(App):
+    def build(self):
+        return MainScreen()
 
-if __name__ == '__main__': #? really neccessary?
+if __name__ == '__main__':
     Calc().run()
