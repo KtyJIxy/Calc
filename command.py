fdea6cfdbc58bd80519c5e02b79ceb409559008c
KtyJIxy <48325606+KtyJIxy@users.noreply.github.com>
2021-01-27 21:24:14 +0200

    Fixed most of the previously failing functions

--- command.py
+++ command.py
@@ -4,10 +4,6 @@ Command functions.
 
 def clear(): return 0
 
-def memorize(a): 
-    m1 = a 
-    return m1
-
 def calculate(*args): exec(*args)
 
 input_from_user = input()
--- functions_main.py
+++ functions_main.py
@@ -23,7 +23,7 @@ def square(a): return a**2
 
 def cube(a): return a**3
 
-def root(a): return a**0.5
+def sqr_root(a): return a**0.5
 
 def procentify(a): return a/100
 
@@ -32,3 +32,16 @@ def procentify(a): return a/100
 def raise_to_y_power(a, y): return a**y
 
 #Other functions
+
+def memorize(a): 
+    m1 = a 
+    return m1
+
+class M_button():
+    def memorize_it(a): 
+        m = a
+
+    def memorize_show():
+        print (m)
+
+M_plus_button = M_button()
